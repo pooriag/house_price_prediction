eebb69c6d24e48b327e11c942ab0ebe1c92b11c3
pog <pooriagolestanipg@gmail.com>
2024-01-25 17:34:59 +0330

    ploted the losses with respect to epochs in training process

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random
-
 import pandas as pnd
 import numpy as np
 import neural_network
+import matplotlib.pyplot as plt
+
 def data_clean(df):
     df = df.replace(to_replace='yes', value=1)
     df = df.replace(to_replace='no', value=0)
@@ -27,11 +28,21 @@ def train(df, start, end, number, iteration):
         sample = random_train_sample(df, start, end, number)
         nn.train(sample[sample.columns[1:]].values.tolist(), sample[sample.columns[0]].values.tolist())
 
+def plot_losses(losses):
+    plt.figure()
+
+    for i in range(0, len(losses), 100):
+        plt.plot(i, losses[i], 'ro')
+    plt.show()
+
+
 df = pnd.read_csv('Housing.csv')
 df = data_clean(df)
 normalized_df = normalizing(df)
 nn = neural_network.house_price_network(12)
-train(normalized_df, 1, 200, 10,  40)
+train(normalized_df, 1, 200, 30,  40)
 for i in range(205, 215):
     print(f'actuale value{df["price"].iloc[i]}')
     print(f'predicted value{nn.predict(normalized_df.iloc[i][1:].values.tolist())}')
+
+plot_losses(nn.losses)
