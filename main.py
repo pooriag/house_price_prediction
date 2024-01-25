f366b59c77411e18a52f0f012c76bd0c2fde1fcc
pog <pooriagolestanipg@gmail.com>
2024-01-25 14:55:37 +0330

    loss and train function had been add to neuaral network data was normalized few bugs on passing data to neural network has been fixed preformance is not so good

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
+import random
+
 import pandas as pnd
 import numpy as np
 import neural_network
+def data_clean(df):
+    df = df.replace(to_replace='yes', value=1)
+    df = df.replace(to_replace='no', value=0)
+    df = df.replace(to_replace='furnished', value=2)
+    df = df.replace(to_replace='semi-furnished', value=1)
+    df = df.replace(to_replace='unfurnished', value=0)
+    #print(df.isnull().any(axis=1))
+    return df
+
+def normalizing(df):
+    mean = df[df.columns[1:]].mean()
+    std = df[df.columns[1:]].std()
+    df[df.columns[1:]] = (df[df.columns[1:]] - mean) / std
+    return df
+
+def random_train_sample(df, start, end, number):
+    return df.iloc[[random.randint(start, end) for i in range(number)]]
+
+def train(df, start, end, number, iteration):
+
+    for i in range(iteration):
+        sample = random_train_sample(df, start, end, number)
+        nn.train(sample[sample.columns[1:]].values.tolist(), sample[sample.columns[0]].values.tolist())
 
 df = pnd.read_csv('Housing.csv')
-
-
-df = df.replace(to_replace='yes', value=1)
-df = df.replace(to_replace='no', value=0)
-df = df.replace(to_replace='furnished', value=2)
-df = df.replace(to_replace='semi-furnished', value=1)
-df = df.replace(to_replace='unfurnished', value=0)
-#print(df['mainroad'])
-list = df.iloc[[0, 1, 2]].values.tolist()
-#print(list)
-nn = neural_network.house_price_network(13)
-nn.predic(list)
+df = data_clean(df)
+normalized_df = normalizing(df)
+nn = neural_network.house_price_network(12)
+train(normalized_df, 1, 200, 10,  40)
+for i in range(205, 215):
+    print(f'actuale value{df["price"].iloc[i]}')
+    print(f'predicted value{nn.predict(normalized_df.iloc[i][1:].values.tolist())}')
--- neural_network.py
+++ neural_network.py
@@ -13,6 +13,25 @@ class house_price_network(nn.Module):
     def forward(self, x):
         return self.layer(x)
 
-    def predic(self, x):
+    def predict(self, x):
         X = torch.Tensor(x)
-        self.layer(X)
+        return self.layer(X)
+
+    def train(self, x, lable):
+        Lable = torch.Tensor(lable)
+        loss_function = nn.MSELoss()
+        learning_rate = (1e-2)
+        epochs = 10000
+
+        optimizer = torch.optim.SGD(self.parameters(), lr=learning_rate)
+
+        for i in range(epochs):
+            y_predict = self.predict(x)
+            loss = loss_function(y_predict.squeeze(), Lable)
+
+            optimizer.zero_grad()
+            loss.backward()
+
+            optimizer.step()
+
+            self.losses.append(loss.item())
